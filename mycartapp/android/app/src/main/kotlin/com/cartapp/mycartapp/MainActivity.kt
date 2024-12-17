adefdb4edfb8ba745f76e607f2881c678792aa88
dilasha-ghimire <dilashhaa@gmail.com>
2024-12-17 21:52:15 +0545

    update: bottom navigation bar

---
+++ mycartapp/android/app/src/main/kotlin/com/cartapp/mycartapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cartapp.mycartapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mycartapp/lib/view/bottom_screen/dashboard.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:mycartapp/view/bottom_screen/about.dart';
+import 'package:mycartapp/view/bottom_screen/cart.dart';
+import 'package:mycartapp/view/bottom_screen/home.dart';
+import 'package:mycartapp/view/bottom_screen/profile.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const HomeScreen(),
+    const CartScreen(),
+    const ProfileScreen(),
+    const AboutScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Cart App'),
+        centerTitle: true,
+      ),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag), label: "Cart"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.album_outlined), label: "About"),
+        ],
+        backgroundColor: const Color.fromARGB(255, 96, 110, 168),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
